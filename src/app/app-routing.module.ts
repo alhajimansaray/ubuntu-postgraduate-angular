ba8382e5738113216c51024e6efac7c09846777b
Alhaji Mansaray <alhajimans2@gmail.com>
2021-08-09 10:46:47 +0000

    Linking the file button in the table to Student-file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from "@angular/router";
 import {HomeComponent} from "./components/home/home.component";
 import {StudentsFormComponent} from "./components/students/students-form/students-form.component";
+import {StudentsFileComponent} from "./components/students/students-file/students-file.component";
 
 
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'students', component: StudentsFormComponent },
+  { path: 'students-file', component: StudentsFileComponent },
   { path: ' ', redirectTo: 'home', pathMatch: 'full' }
 ];
 
