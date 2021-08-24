9f80aa1e61e5787d623b8745eccf2432610194ef
David_Sapunka_Fprnah <david.fornah.ubuntuafrika@gmail.com>
2021-08-24 13:28:23 +0000

    Made changes on Student

--- src/app/components/students/students-form/students-form.component.ts
+++ src/app/components/students/students-form/students-form.component.ts
@@ -19,14 +19,14 @@ export class StudentsFormComponent implements OnInit {
 
   student: Student = new Student();
   newStudent: Student[] = [
-    {id: 1, name: 'Alhaji Mansaray', birthday: '01-01-1990', locality: 'Makeni', year: '2019', university: 'Njala', active: 'Yes'},
-    {id: 2, name: 'David S. Fornah', birthday: '01-01-1986', locality: 'Makeni', year: '2019', university: 'Unimak', active: 'Yes'},
-    {id: 3, name: 'Momoh Sesay', birthday: '01-01-1985', locality: 'Makeni', year: '2020', university: 'Njala', active: 'Yes'},
-    {id: 4, name: 'Edward D. Thoronka', birthday: '01-01-1980', locality: 'Makeni', year: '2020', university: 'Njala', active: 'Yes'},
-    {id: 5, name: 'Andrew O. Kamara', birthday: '01-01-1990', locality: 'Makeni', year: '2019', university: 'Unimak', active: 'Yes'},
-    {id: 6, name: 'John Kamara', birthday: '01-01-1984', locality: 'Makeni', year: '2019', university: 'Unimak', active: 'Yes'},
-    {id: 7, name: 'Gibrilla Kanu', birthday: '01-01-1991', locality: 'Makeni', year: '2019', university: 'Unimak', active: 'Yes'},
-    {id: 8, name: 'Khadijah Bah', birthday: '01-01-1993', locality: 'Makeni', year: '2019', university: 'Unimak', active: 'Yes'}
+    {id: 1, name: 'Alhaji Mansaray', birthday: '01-01-1990', locality: 'Makeni', year: '2021', university: 'Njala', active: 'Yes'},
+    {id: 2, name: 'David S. Fornah', birthday: '01-01-1986', locality: 'Makeni', year: '2021', university: 'Unimak', active: 'Yes'},
+    {id: 3, name: 'Momoh Sesay', birthday: '01-01-1985', locality: 'Makeni', year: '2021', university: 'Njala', active: 'Yes'},
+    {id: 4, name: 'Edward D. Thoronka', birthday: '01-01-1980', locality: 'Makeni', year: '2021', university: 'Njala', active: 'Yes'},
+    {id: 5, name: 'Andrew O. Kamara', birthday: '01-01-1990', locality: 'Makeni', year: '2021', university: 'Unimak', active: 'Yes'},
+    {id: 6, name: 'John Kamara', birthday: '01-01-1984', locality: 'Makeni', year: '2021', university: 'Unimak', active: 'Yes'},
+    {id: 7, name: 'Gibrilla Kanu', birthday: '01-01-1991', locality: 'Makeni', year: '2021', university: 'Unimak', active: 'Yes'},
+    {id: 8, name: 'Khadijah Bah', birthday: '01-01-1993', locality: 'Makeni', year: '2021', university: 'Unimak', active: 'Yes'}
   ];
 
 
